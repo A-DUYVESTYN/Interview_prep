af71f29a10a81a672448741ec92395e5ce5ace22
A-DUYVESTYN <a.duyvestyn@gmail.com>
2022-11-28 09:49:05 -0500

    longest palindrome improved

--- leetcode/longestPalindrome.js
+++ leetcode/longestPalindrome.js
@@ -1,19 +1,38 @@
 // Given a string s, return the longest palindromic substring in s.
 function longestPalindrome(s) {
-    if (!s || s.length < 2)
-        return s;
+    // if (!s || s.length < 2) return s
     var output = "";
-    for (var i = 0; i < (s.length - 1); i++) {
-        for (var j = i + 1; j <= (s.length); j++) {
-            var sub = s.substring(i, j);
-            console.log(sub);
-            if (sub.length > output.length && sub === sub.split('').reverse().join(''))
-                output = sub;
+    function expandFromCentre(l, r) {
+        while (l >= 0 && r < s.length && s[l] === s[r]) {
+            l--;
+            r++;
         }
+        console.log("".concat(s.slice(l + 1, r), "  Left: ").concat(l, ", Right: ").concat(r));
+        return s.slice(l + 1, r);
+    }
+    for (var i = 0; i < (s.length - 1); i++) {
+        console.log('/////////////centre=', i);
+        var subOdd = expandFromCentre(i, i);
+        var subEven = expandFromCentre(i, i + 1);
+        var sub = subEven.length > subOdd.length ? subEven : subOdd;
+        if (sub.length > output.length)
+            output = sub;
     }
     return output;
 }
 ;
+// function longestPalindrome(s: string): string {
+//   if (!s || s.length < 2) return s
+//   let output = ""
+//   for (let i=0; i<(s.length-1); i++) {
+//     for (let j=i+1; j<=(s.length); j++) {
+//       let sub = s.substring(i,j)
+//       console.log(sub)
+//       if (sub.length > output.length && sub === sub.split('').reverse().join('')) output = sub
+//     }
+//   }
+//   return output
+// };
 console.log(longestPalindrome("babad"), "should equal 'bab'");
 console.log(longestPalindrome("ac"), "should equal 'a'");
 console.log(longestPalindrome("bb"), "should equal 'bb'");
--- leetcode/longestPalindrome.ts
+++ leetcode/longestPalindrome.ts
@@ -1,17 +1,39 @@
 // Given a string s, return the longest palindromic substring in s.
 
 function longestPalindrome(s: string): string {
-  if (!s || s.length < 2) return s
+  const len = s.length 
+  if (!s || len < 2) return s
   let output = ""
-  for (let i=0; i<(s.length-1); i++) {
-    for (let j=i+1; j<=(s.length); j++) {
-      let sub = s.substring(i,j)
-      console.log(sub)
-      if (sub.length > output.length && sub === sub.split('').reverse().join('')) output = sub
+  function expandFromCentre(l: number, r: number): string {
+    while (l >= 0 && r < len && s[l] === s[r]) {
+      l--
+      r++
     }
+    return s.slice(l+1,r)
+  }
+  for (let i=0; i<(len-1); i++) {
+    const subOdd = expandFromCentre(i,i)
+    const subEven = expandFromCentre(i,i+1)
+    const sub = subEven.length > subOdd.length ? subEven : subOdd;
+    if (sub.length > output.length) output = sub
   }
   return output
 };
+
+
+// function longestPalindrome(s: string): string {
+//   if (!s || s.length < 2) return s
+//   let output = ""
+  
+//   for (let i=0; i<(s.length-1); i++) {
+//     for (let j=i+1; j<=(s.length); j++) {
+//       let sub = s.substring(i,j)
+//       console.log(sub)
+//       if (sub.length > output.length && sub === sub.split('').reverse().join('')) output = sub
+//     }
+//   }
+//   return output
+// };
 
 console.log(longestPalindrome("babad"), "should equal 'bab'")
 console.log(longestPalindrome("ac"), "should equal 'a'")
